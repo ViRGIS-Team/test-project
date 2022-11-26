9608639604e39499e3446f60ef6fe6de56b9d2b9
runette <runette@gmail.com>
2022-11-26 23:29:06 +0000

    add scri[ts

---
+++ .virgis/conda_linux.sh
@@ -0,0 +1,4 @@
+echo starting conda install
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda.sh
+bash conda.sh -b -p ~/local/miniconda3
+echo completed conda install
