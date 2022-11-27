308ac30fddd96b611407fcc0db85a8f5f3a7c7d3
Paul <runette@gmail.com>
2022-11-27 13:17:40 +0000

    Update conda_macos.sh

--- .virgis/conda_macos.sh
+++ .virgis/conda_macos.sh
@@ -1,4 +1,5 @@
 echo starting conda install
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o conda.sh
 bash conda.sh -b -p ~/local/miniconda3
+echo 'export PATH=~/local/miniconda:$PATH' >>~/.bash_profile
 echo completed conda install
