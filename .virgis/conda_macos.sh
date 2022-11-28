94d1911f0987962ea25c64e40dce2d684a2125cd
Paul <runette@gmail.com>
2022-11-28 11:07:40 +0000

    more debug

--- .virgis/conda_macos.sh
+++ .virgis/conda_macos.sh
@@ -1,8 +1,9 @@
 echo starting conda install
 
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o conda.sh
+bash conda.sh -b -p ~/local/miniconda3
+
 echo "$(ls ~/local/miniconda3)"
 echo "$(ls ~/local/miniconda3/bin)"
 
-curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o conda.sh
-bash conda.sh -b -p ~/local/miniconda3
 echo completed conda install
