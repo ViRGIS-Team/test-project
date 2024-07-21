f28947a09681fa6d97800d005c34902696670ba9
Paul <runette@gmail.com>
2024-07-21 15:35:23 +0100

    update post.sh

--- .virgis/post.sh
+++ .virgis/post.sh
@@ -5,9 +5,8 @@ echo "$(ls Assets/Conda)"
 echo "$(ls Assets/Conda/lib)"
 
 
-select fname in Assets/Conda/*.txt;
-do
+for fname in Assets/Conda/*.txt; do
+echo $fname
 # Don't forget the "" around the second part, else newlines won't be printed
   printf "%s" "$(<$fname)"
-  break
 done
